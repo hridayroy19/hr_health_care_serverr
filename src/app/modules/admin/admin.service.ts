819597906e8b6881d750b9012f2c5013679777a7
hridayroy19 <hrhridoyroy503@gmail.com>
2025-08-21 16:48:52 +0600

    admin seachTrem

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,24 +1,26 @@
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 const getAllFormDB = async (params: any) => {
 
+const andConditaions: Prisma.AdminWhereInput[] = [];
+   const adminSerchAbleFilds = ['name', 'email'];
+    if (params.searchTerm) {
+        andConditaions.push({
+            OR: adminSerchAbleFilds.map(field => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: "insensitive"
+                }
+            }))
+        })
+    }
+
+    console.dir(andConditaions, { depth: "inifinity" });
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditaions };
+
     const result = await prisma.admin.findMany({
-        where: {
-            OR: [{
-                name: {
-                    contains: params.searchTerm,
-                    mode: "insensitive"
-                }
-            },
-            {
-                email: {
-                    contains: params.searchTerm,
-                    mode: "insensitive"
-                }
-            }
-            ]
-        }
+        where: whereConditions
     })
     return result
 }
