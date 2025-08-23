bdafa7d944a9b7f033504332fc1693205c21cdb5
hridayroy19 <hrhridoyroy503@gmail.com>
2025-08-24 00:34:29 +0600

    change password

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
@@ -12,5 +14,16 @@ router.post(
     '/refresh-token',
     AuthController.refreshToken
 )
+
+router.post(
+    '/change-password',
+    auth(
+        UserRole.SUPER_ADMIN,
+        UserRole.ADMIN,
+        UserRole.DOCTOR,
+        UserRole.PATIENT
+    ),
+    AuthController.changePassword
+);
 
 export const AuthRoutes = router;
