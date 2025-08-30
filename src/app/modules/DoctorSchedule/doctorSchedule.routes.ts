329dcb4e3883bfe8307323bb23123fc23217c461
hridayroy19 <hrhridoyroy503@gmail.com>
2025-08-30 23:58:41 +0600

    create doctorshedule

---
+++ src/app/modules/DoctorSchedule/doctorSchedule.routes.ts
@@ -0,0 +1,40 @@
+import { UserRole } from '@prisma/client';
+import express from 'express';
+import { DoctorScheduleController } from './doctorSchedule.controller';
+import auth from '../../middlewares/auth';
+import { DoctorScheduleValidation } from './doctorSchedule.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+/**
+ * API ENDPOINT: /doctor-schedule/
+ * 
+ * Get all doctor schedule with filtering
+ */
+router.get(
+    '/',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+    DoctorScheduleController.getAllFromDB
+);
+
+router.get(
+    '/my-schedule',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.getMySchedule
+)
+
+router.post(
+    '/',
+    auth(UserRole.DOCTOR),
+    validateRequest(DoctorScheduleValidation.create),
+    DoctorScheduleController.insertIntoDB
+);
+
+router.delete(
+    '/:id',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.deleteFromDB
+);
+
+
+export const DoctorScheduleRoutes = router;
